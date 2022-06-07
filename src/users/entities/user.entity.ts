3b2f20e288b3062c7c82b3630194731533bc0a5b
viniciuscrespi <vinicius.crespi@thoughtworks.com>
2022-06-07 17:55:35 -0300

    first commit

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,65 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  DEFAULT = 'default',
+}
+
+@Entity()
+@Unique(['email'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  email: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 200 })
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.DEFAULT,
+  })
+  role: UserRole;
+
+  @Column({ nullable: false, default: true })
+  status: boolean;
+
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: false })
+  salt: string;
+
+  @Column({
+    name: 'confirmation_token',
+    nullable: true,
+    type: 'varchar',
+    length: 64,
+  })
+  confirmationToken: string;
+
+  @Column({
+    name: 'recover_token',
+    nullable: true,
+    type: 'varchar',
+    length: 64,
+  })
+  recoverToken: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
